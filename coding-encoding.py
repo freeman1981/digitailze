e313476409449fcec546527ca6dffd7a380ebb88
aleksandr-kamenev <alexander.i.kamenev@gmail.com>
2024-05-09 23:39:36 +0300

    upd python

--- coding-encoding.py
+++ coding-encoding.py
@@ -1,7 +1,9 @@
-string = "⭐️ утро 123 hello 😁"
+def print_str(string: str) -> None:
+    for char in string:
+        print(char, end='\t- ')
+        for byte in bytes(char, 'utf-8'):
+            print(bin(byte)[2:].zfill(8), end='')
+        print()
 
-for char in string:
-    print(char, end='\t- ')
-    for byte in bytes(char, 'utf-8'):
-        print(bin(byte)[2:].zfill(8), end='')
-    print()
+
+print_str("⭐️ утро 123 hello 😁")
